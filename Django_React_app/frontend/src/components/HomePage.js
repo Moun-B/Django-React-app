cec85b100fe3523481ce34f9fa78f2a8a4e4272e
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-15 19:32:03 +0900

    setting API views

--- Django_React_app/frontend/src/components/HomePage.js
+++ Django_React_app/frontend/src/components/HomePage.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Redirect } from "react-router-dom";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -15,14 +9,14 @@ export default class HomePage extends Component {
   }
 
   render() {
-    return (<Router>
-      <Switch>
-        <Route exact path="/">
-          <p>This is the home page</p>
-        </Route>
-        <Route path="/join" component={RoomJoinPage} />
-        <Route path="/create" component={CreateRoomPage} />
-      </Switch>
-    </Router>)
+    return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<p>This is the home page</p>}></Route>
+        <Route path="/join/*" element={<RoomJoinPage />} />
+        <Route path="/create" element={<CreateRoomPage />} />
+      </Routes>
+    </BrowserRouter>
+    )
   };
 }
