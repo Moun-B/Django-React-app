adb681489a2aa84ff74ca5f3f1ad893504512cf7
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-18 15:19:55 +0900

    migration for Vote

--- Django_React_app/api/models.py
+++ Django_React_app/api/models.py
@@ -10,7 +10,6 @@ def generate_unique_code():
         if Room.objects.filter(code=code).count() == 0:
             break
     return code
-# Create your models here.
 
 class Room(models.Model):
     code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
@@ -18,3 +17,4 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    current_song = models.CharField(max_length=50, null=True)
