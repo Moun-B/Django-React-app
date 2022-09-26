f0f9fa44ed229e6c5cf7d272c0b13adb584b23b5
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-26 16:03:13 +0900

    fast setup

--- Django_React_app/Django_React_app/settings.py
+++ Django_React_app/Django_React_app/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR /'.env')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_DJANGO_KEY')
+SECRET_KEY = 'django-insecure-l5ai^ohedgpfe=632p%t+lq(odmsjoolm&z+ytuq=+89bu@)3y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
