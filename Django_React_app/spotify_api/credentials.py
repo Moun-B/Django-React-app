de4cd349dadd160d7e6d5368dd3d7cf0ef44fd9a
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-09-26 15:34:47 +0900

    modifying credentials

--- Django_React_app/spotify_api/credentials.py
+++ Django_React_app/spotify_api/credentials.py
@@ -1,3 +1,9 @@
-CLIENT_ID = "fe846e3b53874da497c46c5e85df6323"
-CLIENT_SECRET = "720511d44db948c7ad72c890460110b0"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+import os
+from dotenv import load_dotenv
+
+path_to_env_file = ('./.env')
+load_dotenv(path_to_env_file)
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
